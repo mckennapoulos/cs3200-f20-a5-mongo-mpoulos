8bd357d01803f9fd666adbd60a1947c2416f5135
Mckenna Poulos <poulos.m@husky.neu.edu>
2020-12-01 10:58:55 -0800

    small change

--- daos/questions-dao.js
+++ daos/questions-dao.js
@@ -1,8 +1,5 @@
 const questionsModel = require('../models/questions/questions-models')
 const findAllQuestions = () => questionsModel.find()
-
 const findQuestionsForQuiz = (quizId) => questionsModel.find({quizId: quizId})
-
-
 const findQuestionById = (questionId) => questionsModel.findById(questionId)
 module.exports = { findAllQuestions, findQuestionsForQuiz, findQuestionById }
